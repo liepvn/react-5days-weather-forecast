a3036ad948a1593d7c5de6e980725d3601b77d32
Liep Nguyen <liepnguyen@kms-technology.com>
2021-08-16 11:36:06 +0700

    Write more code comment

--- src/shared/apis/weather.api.js
+++ src/shared/apis/weather.api.js
@@ -7,10 +7,24 @@ const axios = Axios.create({
   },
 });
 
+/**
+ * Searches locations by a query string
+ * @see {@link https://www.metaweather.com/api/#locationsearch}
+ *
+ * @param {string} query The query string
+ * @returns {Promise} A promise
+ */
 function searchLocations(query) {
   return axios.get(`/location/search/?query=${query}`);
 }
 
+/**
+ * Fetches forecast data by a location woeid
+ * @see {@link https://www.metaweather.com/api/#location}
+ *
+ * @param {string} query The woeid
+ * @returns {Promise} A promise
+ */
 function fetchForecastByLocation(woeid) {
   return axios.get(`/location/${woeid}/`);
 }
